d1ed7428c82e3ab5438309d044f6246d7457d5f3
rakshit <rakshit1352@gmail.com>
2026-05-21 18:47:30 +0530

    forcing chrome launch during build

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.post('/generate-pdf', async (req, res) => {
 
         headless: true,
 
+        executablePath:
+            '/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome',
+
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
